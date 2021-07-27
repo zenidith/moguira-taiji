43e3233eef010a36d02f95472832eacf42131f31
K Sheenan <kaynesheenan+git@gmail.com>
2021-07-28 00:07:45 +0900

    reset class list for game at beginning, asign random position of mole, add event listeners of 'mouse up'

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ let result = 0
 function randomSquare() {
 
     square.forEach(className => {
-        className.classList
+        className.classList.remove('mole')
     })
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+
+    // assign id of the randomPosition to the hitPosition
+    hitPosition = hitPosition.id
 }
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if(id.id === hitPosition){
+            result = result + 1
+            score.textContent = result
+        }
+    })
+})
