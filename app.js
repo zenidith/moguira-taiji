ef4caf15cc5e687f8a40d6ccf667254293182c7e
K Sheenan <kaynesheenan+git@gmail.com>
2021-07-27 23:59:41 +0900

    started JS

---
+++ app.js
@@ -0,0 +1,13 @@
+const square = document.querySelectorAll('square')
+const mole = document.querySelectorAll('mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+
+let result = 0
+
+function randomSquare() {
+
+    square.forEach(className => {
+        className.classList
+    })
+}
